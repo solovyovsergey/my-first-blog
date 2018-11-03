ed173905086802b462b95cd81ff864f22b99c1c4
Sergey <soloovushka@ya.ru>
2018-11-03 17:12:20 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    re_path(r'^$', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
